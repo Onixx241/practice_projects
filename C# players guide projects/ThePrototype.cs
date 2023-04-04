ce7c5783cae9b9c1dc435e9a276e616b0a068e8c
Onixx241 <102994107+Onixx241@users.noreply.github.com>
2023-04-04 13:50:16 -0400

    indentation etc..

--- C# players guide projects/ThePrototype.cs
+++ C# players guide projects/ThePrototype.cs
@@ -7,28 +7,30 @@ namespace program{
             bool running = true;
 
             while(running){
-                Console.Write("User 1, enter a number between 0 and 100: ");
+                    Console.Write("User 1, enter a number between 0 and 100: ");
                 
-                int number = Convert.ToInt32(Console.ReadLine());
-                Console.Clear();
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
 
-                bool numberEntered = true;
+                    bool numberEntered = true;
 
                 while(numberEntered){
-                    Console.Write("User 2, guess the number ");
 
-                    int answer = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("User 2, guess the number ");
 
-                    if(answer < number){
-                        Console.WriteLine("The answer is too low !");
+                         int answer = Convert.ToInt32(Console.ReadLine());
+
+                         if(answer < number){
+                              Console.WriteLine("The answer is too low !");
                     }
 
-                    if(answer > number){
-                        Console.WriteLine("The answer is too high !");
+                         if(answer > number){
+                              Console.WriteLine("The answer is too high !");
                     }
-                    if(answer == number){
-                        Console.WriteLine("That's the correct answer !");
-                        numberEntered = false;
+                    
+                        if(answer == number){
+                              Console.WriteLine("That's the correct answer !");
+                              numberEntered = false;
                     }
 
                 }
