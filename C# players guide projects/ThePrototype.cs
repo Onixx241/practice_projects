831b77e6e503fe8347bcc71efd503cf61668cb01
Onixx241 <102994107+Onixx241@users.noreply.github.com>
2023-04-04 13:47:56 -0400

    finished

--- C# players guide projects/ThePrototype.cs
+++ C# players guide projects/ThePrototype.cs
@@ -1,1 +1,41 @@
+namespace program{
 
+    public class prog{
+
+        public static void Main(){
+
+            bool running = true;
+
+            while(running){
+                Console.Write("User 1, enter a number between 0 and 100: ");
+                
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                bool numberEntered = true;
+
+                while(numberEntered){
+                    Console.Write("User 2, guess the number ");
+
+                    int answer = Convert.ToInt32(Console.ReadLine());
+
+                    if(answer < number){
+                        Console.WriteLine("The answer is too low !");
+                    }
+
+                    if(answer > number){
+                        Console.WriteLine("The answer is too high !");
+                    }
+                    if(answer == number){
+                        Console.WriteLine("That's the correct answer !");
+                        numberEntered = false;
+                    }
+
+                }
+            } 
+
+        }
+
+    }
+
+}
