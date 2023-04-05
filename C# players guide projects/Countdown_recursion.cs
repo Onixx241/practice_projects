5e9e83e10fd0fe4b7618b9edb8f63df40e611692
Onixx241 <102994107+Onixx241@users.noreply.github.com>
2023-04-05 16:14:39 -0400

    Add files via upload

---
+++ C# players guide projects/Countdown_recursion.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace prog { 
+
+    public class Program {
+
+        public static void recursLoop(int num) {
+            
+            if(num == 0) return;
+
+            
+
+            Console.WriteLine(num);
+            recursLoop(num - 1);
+        }
+
+          public static void Main(string[] args)
+        {
+            recursLoop(20);
+        }
+    
+    }
+
+}
