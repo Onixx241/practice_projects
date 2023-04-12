3a80b57d49e55bddee3793fee2cf5fb9c48078f3
Onixx241 <102994107+Onixx241@users.noreply.github.com>
2023-04-11 23:20:21 -0400

    Add files via upload

---
+++ C# players guide projects/Simulas_test.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace prog { 
+
+    public class Program {
+
+        
+
+        public static void Main(string[] args)
+        {
+            chestState State = chestState.locked;
+
+            
+            while (true)
+            {
+                Console.WriteLine($"The Chest Is {State}, What Do You Want To Do ?");
+                string input = Console.ReadLine();
+
+                if (State == chestState.locked && input == "unlock")
+                    State = chestState.closed;
+                if (State == chestState.closed && input == "open")
+                    State = chestState.open;
+                if (State == chestState.open && input == "close")
+                    State = chestState.closed;
+                if (State == chestState.closed && input == "lock")
+                    State= chestState.locked;
+
+
+            }
+        }
+        enum chestState
+        {
+            locked,
+            closed,
+            open
+            
+
+        }
+    }
+
+}
